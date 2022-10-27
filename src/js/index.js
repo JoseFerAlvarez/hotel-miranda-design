e62a05097bf1aa898f0eca3d6eb4ba1c822f0a94
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-10-27 16:23:50 +0200

    media querys rooms

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
+import Swiper from 'swiper';
+
 document.querySelector(".burger").addEventListener("click", (e) => {
     e.target.classList.toggle("burger--close")
     document.querySelector(".topbar__nav").classList.toggle("topbar__nav--visible");
 });
+
+const swiper = new Swiper(".swiper", {
+    speed: 400,
+    spaceBetween: 100,
+})
