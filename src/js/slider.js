d1fb569f13bbad560d620dc97457bfa2ea0a10dc
JoseFer Alvarez <joseferalvarezromero@gmail.com>
2022-10-27 16:43:16 +0200

    swiperjs added

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 4000,
+    },
+})
